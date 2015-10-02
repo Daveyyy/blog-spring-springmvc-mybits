e335050816f7a9e8afdf02bd6646938c287e12da
alvin <alvin-m@foxmail.com>
2015-10-02 23:42:09 +0800

    搭建环境，编写sql

---
+++ src/main/java/basesql.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS db_blog CHARACTER SET UTF8;
+USE db_blog;
+
+#用户信息表
+CREATE TABLE IF NOT EXISTS t_user(
+user_id int(10) NOT NULL AUTO_INCREMENT COMMENT '用户id',
+user_name VARCHAR(30) NOT NULL UNIQUE COMMENT '用户名',
+password VARCHAR(30) NOT NULL DEFAULT '' COMMENT '密码',
+user_email VARCHAR(30) NOT NULL UNIQUE COMMENT '用户邮箱',
+user_sex VARCHAR(30) NOT NULL COMMENT '用户性别',
+create_time DATETIME NOT NULL COMMENT '用户创建时间',
+user_type int(2) NOT NULL DEFAULT '0' COMMENT '用户类型',
+user_state int(2) NOT NULL DEFAULT '0' COMMENT '用户状态',
+credit int(11) NOT NULL DEFAULT '100' COMMENT '用户积分',
+last_logintime DATETIME DEFAULT NULL COMMENT '用户最后登入时间',
+last_ip VARCHAR(20) DEFAULT NULL COMMENT '用户最后登入ip',
+PRIMARY KEY (`user_id`)
+)ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
